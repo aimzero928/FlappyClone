6944e6a689b4e4eba140e0fa0e3ee5cf2494231d
Aimzero <wa299@naver.com>
2023-10-12 17:27:31 +0900

    포트폴리오
    포트폴리오 업로드

---
+++ Assets/BirdJump.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BirdJump : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float jumpPower; //다른 클래스에서 jumpPower에 접근할 수 있음
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<AudioSource>().Play();
+            rb.velocity = Vector2.up * jumpPower;// (0, 1) * 3
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(Score.score > Score.bestScore){
+            Score.bestScore = Score.score;
+        }
+        SceneManager.LoadScene("GameOverScene");
+    }
+
+}
---
+++ Assets/MakePipe.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakePipe : MonoBehaviour
+{
+    public GameObject pipe;
+    public float timeDiff;
+    float timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > timeDiff)
+        {
+            GameObject newpipe = Instantiate(pipe);
+            newpipe.transform.position = new Vector3(4.5f, Random.Range(-3.1f, 2.2f), 0); // y축의 값은 -3.1~2.2 중 랜덤
+            timer = 0;
+            Destroy(newpipe, 10.0f);
+        }
+     
+    }
+}
